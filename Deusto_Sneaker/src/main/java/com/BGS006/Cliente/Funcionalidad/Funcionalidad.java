a0e4906f3d47b864261466bab8b5eb67e63d92c4
Mikel Muñoz <mikel.maranguren@opendeusto.es>
2022-03-30 10:38:47 +0200

    Método básico en Funcionalidad

--- Deusto_Sneaker/src/main/java/com/BGS006/Cliente/Funcionalidad/Funcionalidad.java
+++ Deusto_Sneaker/src/main/java/com/BGS006/Cliente/Funcionalidad/Funcionalidad.java
@@ -1,5 +1,17 @@
 package com.BGS006.Cliente.Funcionalidad;
 
-public class Funcionalidad {
-    
+	public class Funcionalidad {
+		private static Funcionalidad instance;
+
+		public static Funcionalidad getInstance() {
+			if (instance == null) {
+				instance = new Funcionalidad();
+			}
+
+			return instance;
+		}
+
+		public void Holamundo() {
+			System.out.println("Hola mundo");
+		}
 }
