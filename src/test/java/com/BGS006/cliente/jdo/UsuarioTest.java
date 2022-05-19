7b664fda9253cfd68c0e35bc8d5a0ecd769d3a8d
suhartx <suhartx@opendeusto.es>
2022-05-19 16:45:11 +0200

    Tes UsuarioTest hecho

--- src/test/java/com/BGS006/cliente/jdo/UsuarioTest.java
+++ src/test/java/com/BGS006/cliente/jdo/UsuarioTest.java
@@ -1,17 +1,136 @@
 package com.BGS006.cliente.jdo;
 
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-public class UsuarioTest {
+public class UsuarioTest extends Usuario {
+
+    Usuario u;
 
     @Before
     public void setUp() throws Exception {
+        u = new Usuario("gorka", "gorka","","");
+
+    }
+    @Test
+    public void testGetNombre() {
+        assertEquals("gorka", u.getNombre());
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void testSetNombre() {
+        u.setNombre("Jose");
+        assertEquals("Jose",u.getNombre());
     }
+
+    @Test
+    public void testGetContrasenya() {
+        assertEquals("gorka", u.getContrasenya());
+    }
+
+    @Test
+    public void testSetContrasenya() {
+        u.setContrasenya("12345");
+        assertEquals("12345",u.getContrasenya());
+    }
+
+    @Test
+    public void testGetTarjetaPago() {
+        assertEquals("",u.getTarjetaPago());
+    }
+
+    @Test
+    public void testSetTarjetaPago() {
+        u.setTarjetaPago("hola");
+        assertEquals("hola",u.getTarjetaPago());
+
+    }
+
+    @Test
+    public void testGetRutaAvatar() {
+        assertEquals("", u.getRutaAvatar());
+    }
+
+    @Test
+    public void testSetRutaAvatar() {
+        u.setRutaAvatar("hola");
+        assertEquals("hola",u.getRutaAvatar());
+    }
+
+    @Test
+    public void testGetCarro() {
+        assertNotNull(u.getCarro());
+    }
+
+    @Test
+    public void testSetCarro() {
+        Articulo c = new Articulo("algo", 20, 1, "",2);
+        ArrayList<Articulo> a =  new ArrayList<Articulo>();
+        a.add(c);
+        u.setCarro(a);
+        assertEquals("algo",u.getCarro().get(0).getNombre() );
+    }
+
+    @Test
+    public void testGetCompras() {
+        assertNotNull(u.getCompras());
+
+    }
+
+    @Test
+    public void testSetPedidos() {
+        Compra c = new Compra(1,"gorka", 100);
+        ArrayList<Compra> a =  new ArrayList<Compra>();
+        a.add(c);
+        u.setPedidos(a);
+        assertEquals("gorka",u.getCompras().get(0).getNombreUsuario() );
+
+    }
+
+    @Test
+    public void testAddPedido() {
+        Compra c = new Compra(2,"ibai", 100);
+        u.addPedido(c);
+        assertEquals("ibai", u.getCompras().get(0).getNombreUsuario());
+
+    }
+
+    @Test
+    public void testAddCarrito() {
+        Articulo c = new Articulo("cosas", 20, 2, "",2);
+
+        u.addCarrito(c);
+        assertEquals("cosas",u.getCarro().get(0).getNombre() );
+
+    }
+
+    @Test
+    public void testLimpiarCarrito() {
+        u.limpiarCarrito();
+        assertTrue(u.getCarro().isEmpty());
+    }
+
+    @Test
+    public void testEliminarArticulo() {
+        Articulo c = new Articulo("cosas", 20, 2, "",2);
+
+        u.addCarrito(c);
+        u.eliminarArticulo(0);
+        assertEquals(0,u.getCarro().size());
+    }
+    @Test
+    public void testToString() {
+        String expected = "Usuario{" +
+                "nombre='" + u.getNombre() + '\'' +
+                ", contrasenya='" + u.getContrasenya() + '\'' +
+                ", tarjetaPago='" + u.getTarjetaPago() + '\'' +
+                ", rutaAvatar='" + u.getRutaAvatar() + '\'' +
+                '}';
+        assertEquals(expected, u.toString());
+    }
+
 }
