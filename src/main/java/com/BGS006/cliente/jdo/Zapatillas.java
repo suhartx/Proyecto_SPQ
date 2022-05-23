9309d7e3466dd61eba8fdccc6ab0bf09aaea121a
Gorka de Miguel <72606695+gorkademiguel@users.noreply.github.com>
2022-05-23 23:47:10 +0200

    Documentacion clase Zapatillas.java

--- src/main/java/com/BGS006/cliente/jdo/Zapatillas.java
+++ src/main/java/com/BGS006/cliente/jdo/Zapatillas.java
@@ -4,7 +4,7 @@ import javax.jdo.annotations.InheritanceStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 
 /**
- *
+ * Clase donde se genera los objetos zapatilla
  */
 @PersistenceCapable(detachable = "true")
 @Inheritance(strategy = InheritanceStrategy.NEW_TABLE)
@@ -15,7 +15,7 @@ public class Zapatillas extends Articulo {
 	private String gen;
 
 	/**
-	 *
+	 *constructor con para metros
 	 * @param nombre
 	 * @param precio
 	 * @param id
@@ -27,6 +27,17 @@ public class Zapatillas extends Articulo {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * constructor con parametros
+	 * @param nombre
+	 * @param precio
+	 * @param id
+	 * @param rutaImagen
+	 * @param stock
+	 * @param talla
+	 * @param color
+	 * @param gen
+	 */
 
 	public Zapatillas(String nombre, double precio, long id, String rutaImagen, int stock, int talla, String color,
 					  String gen) {
@@ -36,32 +47,51 @@ public class Zapatillas extends Articulo {
 		this.gen = gen;
 	}
 
-
+/**
+ * recibe la talla
+ */
 	public int getTalla() {
 		return talla;
 	}
 
-
+	/**
+	 * establece la talla
+	 * @param talla
+	 */
 	public void setTalla(int talla) {
 		this.talla = talla;
 	}
 
+	/**
+	 * recibe el color
+	 * @return
+	 */
 
 	public String getColor() {
 		return color;
 	}
 
+	/**
+	 * establece el color
+	 * @param color
+	 */
 
 	public void setColor(String color) {
 		this.color = color;
 	}
 
-
+	/**
+	 * recibe el genero
+	 * @return
+	 */
 	public String getGen() {
 		return gen;
 	}
 
-
+	/**
+	 * establece el genro
+	 * @param gen
+	 */
 	public void setGen(String gen) {
 		this.gen = gen;
 	}
