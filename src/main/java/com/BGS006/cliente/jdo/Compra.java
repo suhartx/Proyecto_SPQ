a2221c1af6552a95a0367909f31f36e0eb63935d
Gorka de Miguel <72606695+gorkademiguel@users.noreply.github.com>
2022-05-23 23:41:34 +0200

    Documentacion clase Compra.java

--- src/main/java/com/BGS006/cliente/jdo/Compra.java
+++ src/main/java/com/BGS006/cliente/jdo/Compra.java
@@ -5,6 +5,10 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 import java.util.ArrayList;
+
+/**
+ * clase donde se generan los objetos compra
+ */
 @PersistenceCapable(detachable = "true")
 public class Compra {
 
@@ -22,8 +26,12 @@ public class Compra {
 	@Join
 	@Persistent(defaultFetchGroup = "true")
 	private ArrayList<Articulo> articulos;
-	
-	
+
+	/**
+	 * Constructor con parametros
+	 * @param articulos
+	 * @param precio
+	 */
 	public Compra(ArrayList<Articulo> articulos, double precio) {
 		super();
 		this.idCompra = count++;
@@ -32,6 +40,13 @@ public class Compra {
 		this.articulos = articulos;
 	}
 
+	/**
+	 * constructor con parametros
+	 * @param id
+	 * @param nombreUsuario
+	 * @param precio
+	 * @param numArticulos
+	 */
 	public Compra(long id, String nombreUsuario,double precio,int numArticulos) {
 		this.idCompra = id;
 		this.nombreUsuario = nombreUsuario;
@@ -40,30 +55,59 @@ public class Compra {
 		this.articulos = new ArrayList<Articulo>();
 	}
 
+	/**
+	 * recibe el nombre de usuario
+	 * @return
+	 */
 	public String getNombreUsuario(){
 		return nombreUsuario;
 	}
 
+	/**
+	 * establece el nombre de usuario
+	 * @param nombreUsuario2
+	 */
 	public void setNombreUsuario(String nombreUsuario2){
 		this.nombreUsuario = nombreUsuario2;
 	}
-	
+
+	/**
+	 * recibe el id de la compra
+	 * @return
+	 */
 	public long getIdCompra() {
 		return idCompra;
 	}
 
+	/**
+	 * recibe el numero de articulos
+	 * @return
+	 */
 	public int getNumArticulo() {
 
 		return articulos.size();
 	}
 
+	/**
+	 * establece el numero de articulos
+	 * @param numero
+	 */
 	public void setNumArticulos(int numero){
 		this.numArticulos = numero;
 	}
+
+	/**
+	 * establece el id de la compra
+	 * @param id
+	 */
 	public void setIdCompra(long id) {
 		this.idCompra = id;
 	}
-	
+
+	/**
+	 * recibe el precio
+	 * @return
+	 */
 	public double getPrecio(){
 		double precio = 0;
 		for (Articulo a:this.articulos) {
@@ -72,24 +116,45 @@ public class Compra {
 
 		return precio;
 	}
-	
+
+	/**
+	 * establece el precio
+	 * @param precio
+	 */
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	
+
+	/**
+	 * recibe una lista de articulos
+	 * @return
+	 */
 	public ArrayList<Articulo> getArticulo() {
 		return articulos;
 	}
-	
+
+	/**
+	 * establece una lista de articulos
+	 * @param articulo
+	 */
 	public void setArticulos(ArrayList<Articulo> articulo) {
 		this.articulos = articulo;
 	}
 
+	/**
+	 * Metodo para añadir articulo
+	 * @param a
+	 */
 	public void anyadirArticulo(Articulo a){
 		this.articulos.add(a);
 		this.precio += a.getPrecio();
 		this.numArticulos += 1;
 	}
+
+	/**
+	 * metodo para quitar articulo
+	 * @param a
+	 */
 	public void quitarArticulo(Articulo a){
 
 		int i = 0;
@@ -102,6 +167,10 @@ public class Compra {
 
 	}
 
+	/**
+	 * recibe el precio
+	 * @return
+	 */
 	public double getPrec() {
 		return precio;
 	}
