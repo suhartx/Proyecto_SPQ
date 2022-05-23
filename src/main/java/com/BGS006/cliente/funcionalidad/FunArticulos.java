5402681e01e9cc8a0d7307c610246fda75b833a6
Ibai <ibaicarracedo@opendeusto.es>
2022-05-23 23:59:36 +0200

    Commit de clases de funcionalidad documentadas y Fix de dependencia.

--- src/main/java/com/BGS006/cliente/funcionalidad/FunArticulos.java
+++ src/main/java/com/BGS006/cliente/funcionalidad/FunArticulos.java
@@ -7,10 +7,17 @@ import com.BGS006.cliente.jdo.Calcetines;
 import com.BGS006.cliente.jdo.Limpiador;
 import com.BGS006.cliente.jdo.Zapatillas;
 
+/**
+ * Clase que define la funcionalidad de articulos de usuario
+ */
 public class FunArticulos {
 	public ArrayList<Articulo> arrayArticulos;
 	private static FunArticulos instance;
 
+	/**
+	 * Crea o devuelve una instancia a la clase
+	 * @return Devuelve el objeto de clase
+	 */
 	public static FunArticulos getInstance() {
 		if (instance == null) {
 			instance = new FunArticulos();
@@ -18,7 +25,13 @@ public class FunArticulos {
 
 		return instance;
 	}
-	
+
+	/**
+	 *
+	 * Comprobacion de un articulo
+	 * @param nom nombre del articulo
+	 * @return Booleano si existe o no
+	 */
 	public boolean comprobarArticulo(String nom) {
 		boolean esta = false;
 		for(int i = 0; i < arrayArticulos.size(); i++) {
@@ -30,8 +43,12 @@ public class FunArticulos {
 		return esta;
 		
 	}
-	
-	
+
+	/**
+	 *
+	 * Añade un calcetin
+	 * @param c calcetin
+	 */
 	public void addCalcetin(Calcetines c) {
 		if(comprobarArticulo(c.getNombre())){
 			System.out.println("El usuario ya est� registrado");
@@ -40,7 +57,13 @@ public class FunArticulos {
 		}
 		
 	}
-	
+
+
+	/**
+	 *
+	 * Añade una zapatilla a la bbdd
+	 * @param z zapatilla que se añade
+	 */
 	public void addZapatilla(Zapatillas z) {
 		if(comprobarArticulo(z.getNombre())){
 			System.out.println("El usuario ya est� registrado");
@@ -48,7 +71,13 @@ public class FunArticulos {
 			arrayArticulos.add(z);
 		}
 	}
-	
+
+
+	/**
+	 *
+	 * Añade un limpiador
+	 * @param l Referencia del objeto limpiador
+	 */
 	public void addLimpiador(Limpiador l) {
 		if(comprobarArticulo(l.getNombre())){
 			System.out.println("El usuario ya est� registrado");
@@ -56,7 +85,13 @@ public class FunArticulos {
 			arrayArticulos.add(l);
 		}
 	}
-	
+
+
+	/**
+	 *
+	 * Elimina el articulo
+	 * @param a Un articulo para eliminar
+	 */
 	public void eliminarArticulo(Articulo a) {
 		if(comprobarArticulo(a.getNombre())){
 			arrayArticulos.remove(a);
@@ -64,7 +99,14 @@ public class FunArticulos {
 			System.out.println("Ese usuario no est� en el sistema");
 		}
 	}
-	
+
+
+	/**
+	 *
+	 * Modifica de un articulo con un stock dado
+	 * @param c Referencia al articulo
+	 * @param stock Número nuevo de stock
+	 */
 	public void modificarStock(Articulo c, int stock) {
 		int posicion = 0;
 		
--- src/main/java/com/BGS006/cliente/funcionalidad/FunUsuario.java
+++ src/main/java/com/BGS006/cliente/funcionalidad/FunUsuario.java
@@ -6,12 +6,18 @@ import com.BGS006.cliente.jdo.Articulo;
 import com.BGS006.cliente.jdo.Usuario;
 
 
-
-
+/**
+ * Clase que define la funcionalidad del usuario
+ */
 public class FunUsuario {
 	public ArrayList<Usuario> arrayUsuarios;
 	private static FunUsuario instance;
 
+
+	/**
+	 * Devuelve una instancia a la clase si existe. Si no la crea.
+	 * @return El objeto de la clase
+	 */
 	public static FunUsuario getInstance() {
 		if (instance == null) {
 			instance = new FunUsuario();
@@ -20,6 +26,13 @@ public class FunUsuario {
 		return instance;
 	}
 
+
+	/**
+	 *
+	 * Comprobamos que un usuario exista
+	 * @param nom Nombre de usuario
+	 * @return True : si existe. False : sino
+	 */
 	public boolean comprobarUsuario(String nom) {
 		boolean esta = false;
 		for (int i = 0; i < arrayUsuarios.size(); i++) {
@@ -32,6 +45,11 @@ public class FunUsuario {
 
 	}
 
+	/**
+	 *
+	 * Creamos un usuario nuevo
+	 * @param u Usuario a crear
+	 */
 	public void CrearUsuario(Usuario u) {
 		if (comprobarUsuario(u.getNombre())) {
 			System.out.println("El usuario ya est� registrado");
@@ -41,6 +59,13 @@ public class FunUsuario {
 		}
 	}
 
+
+	/**
+	 *
+	 * Modifica la contraseña de un usuario concreto
+	 * @param u El usuario a modificar
+	 * @param contra La nueva contraseña
+	 */
 	public void modificarContraUsuario(Usuario u, String contra) {
 
 		int posicion = 0;
@@ -54,6 +79,11 @@ public class FunUsuario {
 		}
 	}
 
+	/**
+	 *
+	 * Elimina un usuario concreto
+	 * @param u El usuario a eliminar
+	 */
 	public void eliminarUsuario(Usuario u) {
 		if (comprobarUsuario(u.getNombre())) {
 			arrayUsuarios.remove(u);
@@ -62,6 +92,11 @@ public class FunUsuario {
 		}
 	}
 
+	/**
+	 * Añadimos un articulo nuevo a la cesta
+	 * @param u El usuario que tiene el carrito
+	 * @param a El articulo que se añade a la cesta
+	 */
 	public void aniadirArticuloACesta(Usuario u, Articulo a) {
 		if (u.getCarro().size() < 100) {
 			u.getCarro().add(a);
